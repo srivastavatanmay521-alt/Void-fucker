da62c040c647be645a59dd6430d4029857e6381d
W  Λ  L  T  Σ  R  † <putulk1972@gmail.com>
2026-05-25 23:35:11 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,43 @@
+# DONT TOUCH IT
+
+SERVER_CONFIG = {
+    "new_name": "REX // 666",  
+    "new_description": "FUCKED BY REX",  
+    "new_icon": "https://i.imgur.com/aL0Q8ty.jpeg"
+}
+
+
+WEBHOOK_CONFIG = {
+    "default_name": "REX // 666",   
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  
+    'channel_type': 'text',   
+    'channel_name': 'REX // 666 x WALTER',  
+    'num_messages': 100000000,   
+    'message_content':  'fucked by walter daddy @everyone https://discord.gg/XFQ8Ba3tQU' 
+}
+
+NO_BAN_KICK_ID = {
+    927989800387096586,       
+    1294173092234526770,     
+}
+
+BOT_PRESENCE = {
+    "type": "watching", 
+    "text": "text" 
+}
+
+EMBED_CONFIG = {
+    "title": "WALTER DADDY IS HERE",    
+    "description": "SUCK DADDY'S DICK BITCH",            
+    "color": 0xFF5733,      
+    "fields": [
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},     
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},
+        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},    
+    ],
+        "footer": "",     
+}
